42342501cb0b4101ed9521c5b0be5fdf1a9fa184
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-06-26 21:35:35 -0400

    refactoring

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"go-server/models"
 	"log"
 	"net/http"
+	"os"
 
-	"../models"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -16,22 +18,33 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// DB connection string
-// for localhost mongoDB
-// const connectionString = "mongodb://localhost:27017"
-const connectionString = "Connection String"
-
-// Database Name
-const dbName = "test"
-
-// Collection name
-const collName = "todolist"
-
 // collection object/instance
 var collection *mongo.Collection
 
 // create connection with mongo db
 func init() {
+	loadTheEnv()
+	createDBInstance()
+}
+
+func loadTheEnv() {
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func createDBInstance() {
+	// DB connection string
+	connectionString := os.Getenv("DB_URI")
+
+	// Database Name
+	dbName := os.Getenv("DB_NAME")
+
+	// Collection name
+	collName := os.Getenv("DB_COLLECTION_NAME")
 
 	// Set client options
 	clientOptions := options.Client().ApplyURI(connectionString)
